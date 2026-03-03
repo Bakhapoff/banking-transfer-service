b51e52b72af7ae15ad5de34398c3e7ed92e68a46
Bektur Bakhapov <bekturbakhapov@MacBook-Air-Bektur.local>
2026-03-03 20:54:30 +0600

    initial project

---
+++ src/main/java/kg/ab/transfer/BankingTransferServiceApplication.java
@@ -0,0 +1,13 @@
+package kg.ab.transfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingTransferServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingTransferServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kg/ab/transfer/BankingTransferServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.ab.transfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingTransferServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
